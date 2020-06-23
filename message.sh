530bc4605a07beddc957434fcc7f1264583efaa9
Christian Dupuis <cd@atomist.com>
2020-06-23 22:45:11 +0200

    Update message.sh

--- message.sh
+++ message.sh
@@ -3,7 +3,7 @@ echo '{
         "say-it-in-slack-skill"
     ],
     "body": {
-    "fallback": "test",
+    "text": "fallback",
     "blocks": "[\n        {\n            \"type\": \"section\",\n            \"text\": {\n                \"type\": \"mrkdwn\",\n                \"text\": \"Hello, this is a block message\"\n            }\n        },\n        {\n            \"type\": \"divider\"\n        }\n    ]"
 }
 }' > $ATOMIST_MESSAGES_DIR/test.json
