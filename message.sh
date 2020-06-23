ca619e5e9401410f4790af32ec1cdb4ee5f38f01
Christian Dupuis <cd@atomist.com>
2020-06-23 22:17:18 +0200

    Update message.sh

--- message.sh
+++ message.sh
@@ -3,18 +3,18 @@ echo '{
         "cd"
     ],
     "body": {
-        "fallback": "Hello, this is a block message",
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Hello, this is a block message"
-                }
-            },
-            {
-                "type": "divider"
-            }
-        ]
-    }
+	"attachments": [],
+	"blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Hello, this is a block message"
+			}
+		},
+		{
+			"type": "divider"
+		}
+	]
+}
 }' > $ATOMIST_MESSAGES_DIR/test.json
